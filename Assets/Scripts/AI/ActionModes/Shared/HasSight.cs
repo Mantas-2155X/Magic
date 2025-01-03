55baac5dec2774d8e1def4dcbe8ba4eb6c748f07
Mantas-2155X <13873974+Mantas-2155X@users.noreply.github.com>
2025-01-03 15:56:34 +0200

    Improve perf of sight check

--- Assets/Scripts/AI/ActionModes/Shared/HasSight.cs
+++ Assets/Scripts/AI/ActionModes/Shared/HasSight.cs
@@ -26,7 +26,12 @@ namespace AI.ActionModes.Shared
 			var direction = target.position - position;
 			var originCenter = position + transform.up * 0.5f;
 
-			if (!Physics.defaultPhysicsScene.Raycast(originCenter, direction, out var hit, float.MaxValue, ~LayerMaskTools.GetMask(), QueryTriggerInteraction.Ignore))
+			if (!Physics.Raycast(originCenter, direction, out var hit, float.MaxValue, ~LayerMaskTools.GetMask(), QueryTriggerInteraction.Ignore))
+				return false;
+
+			// Check if the hit is the target so we don't waste extra casts calls
+			var hitTransform = hit.collider.transform;
+			if (hitTransform != target)
 				return false;
 
 			// Projectiles have thickness so we need to cast more rays to make sure the projectile isn't going to just hit a wall. Spherecast does not work here because it spawns inside a collider and therefore it ignores the wall 
@@ -35,16 +40,16 @@ namespace AI.ActionModes.Shared
 				var instance = hit.colliderInstanceID;
 				var originRight = originCenter + transform.right * 0.4f;
 				
-				if (!Physics.defaultPhysicsScene.Raycast(originRight, direction, out var hitRight, hit.distance + 1f, ~LayerMaskTools.GetMask(), QueryTriggerInteraction.Ignore) || hitRight.colliderInstanceID != instance)
+				if (!Physics.Raycast(originRight, direction, out var hitRight, hit.distance + 1f, ~LayerMaskTools.GetMask(), QueryTriggerInteraction.Ignore) || hitRight.colliderInstanceID != instance)
 					return false;
 				
 				var originLeft = originCenter - transform.right * 0.4f;
 				
-				if (!Physics.defaultPhysicsScene.Raycast(originLeft, direction, out var hitLeft, hit.distance + 1f, ~LayerMaskTools.GetMask(), QueryTriggerInteraction.Ignore) || hitLeft.colliderInstanceID != instance)
+				if (!Physics.Raycast(originLeft, direction, out var hitLeft, hit.distance + 1f, ~LayerMaskTools.GetMask(), QueryTriggerInteraction.Ignore) || hitLeft.colliderInstanceID != instance)
 					return false;
 			}
 			
-			return hit.collider.transform == target;
+			return true;
 		}
 	}
 }
