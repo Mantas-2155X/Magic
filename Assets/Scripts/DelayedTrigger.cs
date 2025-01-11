c754607628d42f7cab518c79dbee2798a5e45b0c
Mantas-2155X <13873974+Mantas-2155X@users.noreply.github.com>
2025-01-11 15:59:09 +0200

    Automatic elevator trigger for player

---
+++ Assets/Scripts/DelayedTrigger.cs
@@ -0,0 +1,62 @@
+using Events;
+using Tools;
+using UnityEngine;
+
+public class DelayedTrigger : MonoBehaviour
+{
+	[SerializeField]
+	public OnTriggerEvent OnTriggerEvent;
+	
+	[SerializeField]
+	public bool IsMultiTrigger;
+
+	[SerializeField]
+	public float TriggerAfter;
+
+	private bool triggered;
+
+	private float enterTime = -1f;
+	private Collider enterCollider;
+
+	public void Update()
+	{
+		if (triggered || enterTime < 0f)
+			return;
+
+		if (Time.time < enterTime + TriggerAfter)
+			return;
+		
+		if (!IsMultiTrigger)
+			triggered = true;
+			
+		enterTime = -1f;
+		enterCollider = null;
+			
+		OnTriggerEvent.Invoke(enterCollider);
+	}
+	
+	public void OnTriggerEnter(Collider other)
+	{
+		if (triggered)
+			return;
+		
+		enterTime = Time.time;
+		enterCollider = other;
+	}
+	
+	public void OnTriggerExit(Collider other)
+	{
+		if (triggered)
+			return;
+
+		enterTime = -1f;
+		enterCollider = null;
+	}
+
+#if UNITY_EDITOR
+	public void OnDrawGizmos()
+	{
+		EventTools.DrawListeners(transform, OnTriggerEvent, Color.blue);
+	}
+#endif
+}
