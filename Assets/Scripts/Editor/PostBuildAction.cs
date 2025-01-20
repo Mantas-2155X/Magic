96e51042702d8e56b9a55702f719936e655c514d
Mantas-2155X <13873974+Mantas-2155X@users.noreply.github.com>
2025-01-20 14:19:55 +0200

    Move localization after build

---
+++ Assets/Scripts/Editor/PostBuildAction.cs
@@ -0,0 +1,68 @@
+using System;
+using System.IO;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using File = System.IO.File;
+
+namespace Editor
+{
+	public class PostBuildAction : IPostprocessBuildWithReport
+	{
+		public int callbackOrder { get; }
+
+		private readonly string[] copyAssets = { "localization" };
+		
+		public void OnPostprocessBuild(BuildReport report)
+		{
+			var executable = new FileInfo(report.summary.outputPath);
+			if (!executable.Exists || executable.Directory == null)
+				return;
+			
+			var directory = executable.Directory.FullName;
+
+			foreach (var copyAsset in copyAssets)
+			{
+				if (!File.Exists(copyAsset) && !Directory.Exists(copyAsset))
+					continue;
+
+				var attributes = File.GetAttributes(copyAsset);
+				if ((attributes & FileAttributes.Directory) == FileAttributes.Directory)
+				{
+					Copy(copyAsset, Path.Combine(directory, copyAsset));
+				}
+				else
+				{
+					File.Copy(copyAsset, Path.Combine(directory, copyAsset));
+				}
+			}
+		}
+		
+		public static void Copy(string source, string target)
+		{
+			var sourceDirectory = new DirectoryInfo(source);
+			var targetDirectory = new DirectoryInfo(target);
+
+			CopyAll(sourceDirectory, targetDirectory);
+		}
+
+		public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+		{
+			Directory.CreateDirectory(target.FullName);
+
+			foreach (var file in source.GetFiles())
+			{
+				if (file.Name.StartsWith("."))
+					continue;
+				
+				Console.WriteLine($"Copying {target.FullName}\\{file.Name}");
+				file.CopyTo(Path.Combine(target.FullName, file.Name), true);
+			}
+
+			foreach (var sourceSubdirectory in source.GetDirectories())
+			{
+				var targetSubdirectory = target.CreateSubdirectory(sourceSubdirectory.Name);
+				CopyAll(sourceSubdirectory, targetSubdirectory);
+			}
+		}
+	}
+}
