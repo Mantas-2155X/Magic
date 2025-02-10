74666b70967a0db56ccdda2e179feb5b7caf6e8a
Mantas-2155X <13873974+Mantas-2155X@users.noreply.github.com>
2025-02-10 18:30:46 +0200

    Button indicator

--- Assets/Scripts/Objects/Base/BaseButton.cs
+++ Assets/Scripts/Objects/Base/BaseButton.cs
@@ -1,4 +1,6 @@
+using System.Threading;
 using AI.Interfaces;
+using Cysharp.Threading.Tasks;
 using Objects.Events;
 using Tools;
 using UnityEngine;
@@ -9,6 +11,24 @@ namespace Objects.Base
 	{
 		[SerializeField]
 		public OnButtonUsedEvent OnButtonUsedEvent = new ();
+
+		[SerializeField]
+		public float IndicateDuration;
+		
+		private static readonly int emissionColor = Shader.PropertyToID("_EmissionColor");
+
+		private CancellationTokenSource cancellationToken = new ();
+
+		private Material sideMaterial;
+		
+		public override void Awake()
+		{
+			base.Awake();
+
+			var rend = GetComponent<Renderer>();
+			var mats = rend.materials;
+			sideMaterial = mats[1];
+		}
 		
 		public override bool Use(IAlive user)
 		{
@@ -16,6 +36,11 @@ namespace Objects.Base
 			if (!success)
 				return false;
 
+			cancellationToken?.Cancel();
+			cancellationToken = new CancellationTokenSource();
+			
+			indicate(cancellationToken.Token).Forget();
+			
 			OnButtonUsedEvent?.Invoke();
 			return true;
 		}
@@ -26,5 +51,23 @@ namespace Objects.Base
 			EventTools.DrawListeners(transform, OnButtonUsedEvent, Color.blue);
 		}
 #endif
+
+		private async UniTaskVoid indicate(CancellationToken token)
+		{
+			if (token.IsCancellationRequested)
+				return;
+			
+			sideMaterial.color = Color.green;
+			sideMaterial.SetColor(emissionColor, Color.green * 1.25f);
+			sideMaterial.EnableKeyword("_EMISSION");
+			
+			await UniTask.WaitForSeconds(IndicateDuration, cancellationToken: token);
+			
+			if (cancellationToken.IsCancellationRequested)
+				return;
+			
+			sideMaterial.color = Color.black;
+			sideMaterial.DisableKeyword("_EMISSION");
+		}
 	}
 }
