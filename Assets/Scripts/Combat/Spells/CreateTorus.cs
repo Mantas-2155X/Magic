87cb31ee8cc56410435bd4af01764530d0bfe026
Mantas-2155X <13873974+Mantas-2155X@users.noreply.github.com>
2025-02-25 01:37:55 +0200

    Create torus spell

---
+++ Assets/Scripts/Combat/Spells/CreateTorus.cs
@@ -0,0 +1,53 @@
+using System;
+using AI;
+using AI.Base;
+using AI.Interfaces;
+using Combat.Spells.Base;
+using Managers;
+using ScriptableObjects;
+using UnityEngine;
+
+namespace Combat.Spells
+{
+	public class CreateTorus : BaseSpell
+	{
+		public override bool FinishCasting()
+		{
+			if (!base.FinishCasting())
+				return false;
+
+			var core = Owner.Body.Core;
+			var spawnPos = core.position + core.forward;
+			
+			var torus = AIManager.Instance.CreateNPC(spawnPos, Vector3.zero, (NPCData)ObjectManager.Instance.GetAlive("AI_TORUS_NAME"), Owner.RelationshipGroup);
+
+			BaseAlive target = null;
+			
+			switch (Owner)
+			{
+				case Player:
+				{
+					var rb = LastHit.rigidbody;
+					if (rb != null)
+						target = rb.GetComponent<BaseAlive>();
+					break;
+				}
+				case NPC npc:
+				{
+					if (npc.AttackTarget != null && npc.AttackTarget is BaseAlive alive)
+						target = alive;
+					break;
+				}
+				default:
+					throw new NotImplementedException();
+			}
+
+			torus.Wander();
+			
+			if (target != null)
+				torus.AssignAttackTarget(target);
+			
+			return true;
+		}
+	}
+}
