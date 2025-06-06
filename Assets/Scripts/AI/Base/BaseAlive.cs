1ecd84f07877641834482ce923f954f3d054644f
Mantas-2155X <13873974+Mantas-2155X@users.noreply.github.com>
2025-06-06 20:30:59 +0300

    Fix bad logic from last commit

--- Assets/Scripts/AI/Base/BaseAlive.cs
+++ Assets/Scripts/AI/Base/BaseAlive.cs
@@ -372,7 +372,7 @@ namespace AI.Base
 				return;
 
 			var type = data.Type == "" ? typeof(BaseSpell) : Type.GetType(data.Assembly == "" ? data.Type : $"{data.Type}, {data.Assembly}");
-			if (type == null || typeof(ISpell).IsAssignableFrom(type))
+			if (type == null || !typeof(ISpell).IsAssignableFrom(type))
 			{
 				Debug.LogError($"[BaseAlive] Failed to learn spell {data.Name} as the custom type is not valid");
 				return;
